1e347fcb65bb094339208e1082c68295b7ec78c2
tuanpn.tb <45645698+tuanpnUET@users.noreply.github.com>
2020-08-28 20:28:46 +0700

    Update HelloController.java

--- Buoi1/src/main/java/com/itplus/HelloController.java
+++ Buoi1/src/main/java/com/itplus/HelloController.java
@@ -1,5 +1,14 @@
 package com.itplus;
 
-public class HelloController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class HelloController {
+	@RequestMapping("/Hello")
+	public String sayHello(ModelMap map) {
+		map.addAttribute("msg","Hello Spring MVC");
+		return "Hello";
+	}
 }
