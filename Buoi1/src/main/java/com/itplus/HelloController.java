fbaed60bd582795a5fc150abbddcaddf808826dd
tuanpn.tb <45645698+tuanpnUET@users.noreply.github.com>
2020-09-07 18:56:39 +0700

    Update HelloController.java

--- Buoi1/src/main/java/com/itplus/HelloController.java
+++ Buoi1/src/main/java/com/itplus/HelloController.java
@@ -5,6 +5,8 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
@@ -19,4 +21,5 @@ public class HelloController {
 		request.setAttribute("name", hoten);
 		return "display";
 	}
+	
 }
