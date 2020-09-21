c1e4ce0d31f3b9763a527d0d3dd7ba7f9b92a4af
tuanpn.tb <45645698+tuanpnUET@users.noreply.github.com>
2020-09-21 18:30:44 +0700

    Create HelloController.java

---
+++ Buoi1/src/main/java/com/itplus/controller/HelloController.java
@@ -0,0 +1,25 @@
+package com.itplus.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+	@RequestMapping("/Hello")
+	public String sayHello(ModelMap map) {
+		map.addAttribute("msg","Hello Spring MVC");
+		return "Hello";
+	}
+	@RequestMapping("/get-name")
+	public String getName(HttpServletRequest request) {
+		String hoten = "Tuan Pham Ngoc";
+		request.setAttribute("name", hoten);
+		return "display";
+	}
+	
+}
