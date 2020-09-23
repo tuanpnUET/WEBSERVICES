38c9afd0c881768cd5504b0cafeda61e0a7e9202
tuanpn.tb <45645698+tuanpnUET@users.noreply.github.com>
2020-09-23 20:03:33 +0700

    Create User.java

---
+++ Java1802e_Hello/src/main/java/com/itplus/entity/User.java
@@ -0,0 +1,37 @@
+package com.itplus.entity;
+
+public class User {
+	private int id;
+	private String ten_kh;
+	private String so_dt;
+	private int tuoi;
+	public User() {		
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTen_kh() {
+		return ten_kh;
+	}
+	public void setTen_kh(String ten_kh) {
+		this.ten_kh = ten_kh;
+	}
+	public String getSo_dt() {
+		return so_dt;
+	}
+	public void setSo_dt(String so_dt) {
+		this.so_dt = so_dt;
+	}
+	public int getTuoi() {
+		return tuoi;
+	}
+	public void setTuoi(int tuoi) {
+		this.tuoi = tuoi;
+	}
+	
+	
+	
+}
